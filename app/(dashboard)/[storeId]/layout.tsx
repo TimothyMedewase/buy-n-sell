31a21ffbbe97aef2bc9a3bb1a2d4965bd6ae7b9e
Timothy Medewase <medewaset@gmail.com>
2025-01-28 14:27:27 -0600

    new

---
+++ app/(dashboard)/[storeId]/layout.tsx
@@ -0,0 +1,36 @@
+import prismadb from "@/lib/prismadb";
+import { auth } from "@clerk/nextjs/server";
+import { redirect } from "next/navigation";
+import Navbar from "@/components/navbar";
+
+export default async function DashboardLayout({
+  children,
+  params,
+}: {
+  children: React.ReactNode;
+  params: { storeId: string };
+}) {
+  const { userId } = await auth();
+
+  if (!userId) {
+    redirect("/sign-in");
+  }
+  //Route "/[storeId]" used `params.storeId`. `params` should be awaited before using its properties.
+  const store = await prismadb.store.findFirst({
+    where: {
+      id: params.storeId,
+      userId: userId,
+    },
+  });
+
+  if (!store) {
+    redirect("/");
+  }
+
+  return (
+    <>
+      <Navbar />
+      {children}
+    </>
+  );
+}
